1b0b24157d21fc71e9b949e4e79d0d9bfc9b295b
DevShasa <shasa.thuo@gmail.com>
2024-04-09 00:35:37 +0300

    Generate endpoint

--- src/App.js
+++ src/App.js
@@ -7,41 +7,53 @@ const UPDATE_ENDPOINT = "https://api-uat.craneww.com/ws/rest/uat/v0/craneReferen
 function App() {
 
   const [lookup, setLookup] = useState("")
-  const [generate, setGenerate] = useState("")
+  const [generate, setGenerate] = useState({
+    client:"",
+    Unit:"",
+    count:""
+  })
   const [update, setUpdate] = useState("")
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false)
 
 
-  async function submitLookup(){
-    if(!lookup){
-      alert("LOOKUP INPUT IS EMPTY")
-      return
-    }
-
-    try {
-      const results = await fetch(LOOKUP_ENDPOINT+lookup, {
-        method:"GET",
-        headers:{
-          'Content-Type': 'application/json'
-        },
-      })
-      if(results.ok){
-        // query is successful 
-        const data = await results.json()
-        console.log(data) // display the data here ?  
-      }
-      throw new Error
-    } catch (error) {
-      alert("SOMETHING WENT WRONG SUBMITTING LOOKUP")
-      console.log("error")
-    }
+  function captureGenerateInput(e){
+    setGenerate(prev=>({
+      ...prev,
+      [e.target.name]:e.target.value
+    }))
   }
 
+  // async function submitLookup(){
+  //   if(!lookup){
+  //     alert("LOOKUP INPUT IS EMPTY")
+  //     return
+  //   }
+
+  //   try {
+  //     const results = await fetch(LOOKUP_ENDPOINT+lookup, {
+  //       method:"GET",
+  //       headers:{
+  //         'Content-Type': 'application/json'
+  //       },
+  //     })
+  //     if(results.ok){
+  //       // query is successful 
+  //       const data = await results.json()
+  //       console.log(data) // display the data here ?  
+  //     }
+  //     throw new Error
+  //   } catch (error) {
+  //     alert("SOMETHING WENT WRONG SUBMITTING LOOKUP")
+  //     console.log("error")
+  //   }
+  // }
+
   async function subbmitGenerate(){
-    if(!lookup){
-      alert("LOOKUP INPUT IS EMPTY")
+    if(!generate.Unit || !generate.client || !generate.count){
+      alert("ONE OR MORE INPUTS FOR GENERATE IS EMPTY")
       return
     }
+
 
     try {
       const results = await fetch(GENERATE_ENDPOINT, {
@@ -52,12 +64,13 @@ function App() {
         body:JSON.stringify(generate)
       })
 
+      console.log("GENERATE ENDPOINT REQUEST:::", results)
       if(results.ok){
         const data = await results.json()
-        console.log(data) 
+        console.log("GENERATE ENDPOINT DATA:::",data) 
       }
 
-      throw new Error
+      throw new Error()
 
     } catch (error) {
       alert("SOMETHING WENT WRONG SUBMITTING GENERATE")
@@ -65,33 +78,33 @@ function App() {
     }
   }
 
-  async function submitUpdate(){
-    if(!lookup){
-      alert("LOOKUP INPUT IS EMPTY")
-      return
-    }
+  // async function submitUpdate(){
+  //   if(!lookup){
+  //     alert("LOOKUP INPUT IS EMPTY")
+  //     return
+  //   }
 
-    try {
-      const results = await fetch(UPDATE_ENDPOINT, {
-        method:"PUT",
-        headers:{
-          'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(generate)
-      })
+  //   try {
+  //     const results = await fetch(UPDATE_ENDPOINT, {
+  //       method:"PUT",
+  //       headers:{
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body:JSON.stringify(generate)
+  //     })
 
-      if(results.ok){
-        const data = await results.json()
-        console.log(data) 
-      }
+  //     if(results.ok){
+  //       const data = await results.json()
+  //       console.log(data) 
+  //     }
 
-      throw new Error
+  //     throw new Error
 
-    } catch (error) {
-      alert("SOMETHING WENT WRONG SUBMITTING UPDATE")
-      console.log("error")
-    }
-  }
+  //   } catch (error) {
+  //     alert("SOMETHING WENT WRONG SUBMITTING UPDATE")
+  //     console.log("error")
+  //   }
+  // }
 
   return (
     <div className="container">
@@ -100,7 +113,7 @@ function App() {
         </div>
         <div className="contents">
           <div className="content_search_inputs">
-            <div className="input_section">
+            {/* <div className="input_section">
                 <p>Lookup</p>
                 <input 
                   type="text"
@@ -123,16 +136,40 @@ function App() {
                 <button onClick={submitUpdate}>
                   Submit Update
                 </button>    
-            </div>
+            </div> */}
             <div className="input_section">
                 <p>Generate</p>
-                <select value={generate} onChange={(e)=>setGenerate(e.target.value)}>
+                {/* <select value={generate} onChange={(e)=>setGenerate(e.target.value)}>
                   <option value="">Select an option</option>
                   <option value="Option 1">Select option one</option>
                   <option value="Option 2">Select option two</option>
                   <option value="Option 3">Select option three</option>
                   <option value="Option 4">Select option four</option>
-                </select>
+                </select> */}
+                <p id="label">Client</p>
+                <input 
+                  type="text"
+                  placeholder="Client"
+                  value={generate.client}
+                  onChange={(e)=>captureGenerateInput(e)}
+                  name="client"
+                />
+                <p id="label">Unit</p>
+                <input 
+                  type="text"
+                  placeholder="unit"
+                  value={generate.Unit}
+                  onChange={(e)=>captureGenerateInput(e)}
+                  name="Unit"
+                />
+                <p id="label">Count</p>
+                <input 
+                  type="number"
+                  placeholder="count"
+                  value={generate.count}
+                  onChange={(e)=>captureGenerateInput(e)}
+                  name="count"
+                />
                 <button onClick={subbmitGenerate}>
                   Submit Generate
                 </button>    
