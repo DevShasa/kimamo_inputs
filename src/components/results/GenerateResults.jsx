ef30d216eed7541ef62b396a702a7823e282f7fe
DevShasa <shasa.thuo@gmail.com>
2024-04-09 15:43:43 +0300

    Feat: display results

--- src/components/results/GenerateResults.jsx
+++ src/components/results/GenerateResults.jsx
@@ -4,10 +4,24 @@ import PropTypes from 'prop-types'
 function GenerateResults(props) {
     const {data} = props
 
-    console.log("DATA FROM GENERATE--->", data)
+    console.log("DATA FROM GENERATE--->", data.queryResults.references)
+
 
   return (
-    <div>{JSON.stringify(data)}</div>
+    <div className='results_container'>
+        <div className='results_top'>
+            <p>Status: {data?.queryResults?.Status}</p>
+            <p>{` - `}</p>
+            <p>Created Time: {data?.queryResults?.createdtime}</p>
+        </div>
+        <div>
+            {data.queryResults.references.map((item)=>{
+                return (
+                    <div key={item}>{item}</div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
